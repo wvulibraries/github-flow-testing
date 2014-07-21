41a1290caf787624fe95b09b5ed0ed9a0ebb181e
Michael Bond <mbond@the-forgotten.org>
2014-07-21 09:16:29 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<html>
+
+<head>
+	<title>Github flow testing</title>
+</head>
+
+<body>
+
+	<p>
+		This file will outline changes that we want to test. A strike should be put through the list items that have been completed.
+	</p>
+
+	<ul>
+		<li>Add a PHP file to this project, include it, print the results below</li>
+		<li>2 people make changes to both files, avoid conflicts</li>
+		<li>2 people make changes to included PHP file, intentionally create a conflict</li>
+		<li>1 person adds a new feature, 1 person changes directory structure.</li>
+	</ul>
+
+	<hr />
+
+	<h2>Testing Area</h2>
+
+</body>
+
+</html>
